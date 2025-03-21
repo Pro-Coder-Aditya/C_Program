38a6634f294bf0114895d2cc2e99b0fea68d190a
Just Aditya <mungekar.aditya15@gmail.com>
2025-03-21 09:36:53 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,18 @@
+// first C program
+// # preprocessor
+
+#include <stdio.h> // header file
+
+int a = 10; // global declaration section
+
+int main() 
+{
+  
+    printf("Hello World\n"); // output
+    printf("Rajes\n");    
+
+    printf("%d",a);
+
+    return 0;
+    
+}
